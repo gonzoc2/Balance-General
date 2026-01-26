7485dd11b45912669bf8061486163b77fb4208c3
gonzoc2 <gcortes@esgari.com.mx>
2026-01-26 13:45:57 -0600

    Update balance pruebas.py

--- balance pruebas.py
+++ balance pruebas.py
@@ -26,7 +26,7 @@ st.markdown(
 
 
 EMPRESAS = ["HOLDING", "FWD", "WH", "UBIKARGA", "EHM", "RESA", "GREEN"]
-COLUMNAS_CUENTA = ["Descripción"]
+COLUMNAS_CUENTA = ["Cuenta","Descripción"]
 NUMERO_CUENTA = ["Cuenta"]
 COLUMNAS_MONTO = ["Saldo final", "Saldo"]
 CLASIFICACIONES_PRINCIPALES = ["ACTIVO", "PASIVO", "CAPITAL"]
@@ -551,6 +551,7 @@ elif selected == "BALANCE POR EMPRESA":
 
 
    
+
 
 
 
